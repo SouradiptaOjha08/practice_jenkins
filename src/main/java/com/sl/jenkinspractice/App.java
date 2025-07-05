df229dc5dacde5007d47d7eaf2ad053ec694ad62
SouradiptaOjha08 <souradipta2021@gmail.com>
2025-07-05 07:34:39 +0530

    adding some extra lines

--- src/main/java/com/sl/jenkinspractice/App.java
+++ src/main/java/com/sl/jenkinspractice/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	int a=10;
+    	int b=20;
+    	System.out.println("sum of both variable is" + " " + (a+b));
         System.out.println( "Hello World!" );
     }
 }
